e46fa772341ab37fb11c2b90b288ad433a8130ae
juber dev <juberdev01@gmail.com>
2023-03-08 00:22:54 -0500

    button_navigation

--- lib/screens/home_screem.dart
+++ lib/screens/home_screem.dart
@@ -1,4 +1,5 @@
 import 'package:disenos_app/widgets/background.dart';
+import 'package:disenos_app/widgets/custom_button_navigation.dart';
 import 'package:disenos_app/widgets/pague_title.dart';
 import 'package:flutter/material.dart';
 
@@ -8,12 +9,14 @@ class HomeScreem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(
-      children: const [
-        Background(),
-        _HomeBody(),
-      ],
-    ));
+      body: Stack(
+        children: const [
+          Background(),
+          _HomeBody(),
+        ],
+      ),
+      bottomNavigationBar: CustonButtonNavigation(),
+    );
   }
 }
 
---
+++ lib/widgets/custom_button_navigation.dart
@@ -0,0 +1,33 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class CustonButtonNavigation extends StatelessWidget {
+  const CustonButtonNavigation({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      showSelectedLabels: false,
+      showUnselectedLabels: false,
+      selectedItemColor: Colors.pink,
+      backgroundColor: const Color.fromRGBO(55, 57, 84, 1),
+      unselectedItemColor: const Color.fromRGBO(116, 117, 152, 1),
+      currentIndex: 2,
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.calendar_today_outlined),
+          label: 'calendario',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.pie_chart_outline_outlined),
+          label: 'grafica',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.supervised_user_circle),
+          label: 'usuario',
+        ),
+      ],
+    );
+  }
+}
