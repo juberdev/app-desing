cb7cddb176235b4119763832c72b2cf1a46c78cf
juber dev <juberdev01@gmail.com>
2023-03-08 18:10:37 -0500

    blur

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Material App',
       initialRoute: 'home_screen',
+      theme: ThemeData.dark(),
       routes: {
         'basicDesign': (_) => const BasicDesingScreen(),
         'Scroll_desing': (_) => const Scroll_desing(),
--- lib/widgets/card_table.dart
+++ lib/widgets/card_table.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:ui';
 
 class CardTable extends StatelessWidget {
   const CardTable({super.key});
@@ -7,6 +8,62 @@ class CardTable extends StatelessWidget {
   Widget build(BuildContext context) {
     return Table(
       children: const [
+        TableRow(
+          children: [
+            _SingleCard(
+              color: Colors.blue,
+              icon: Icons.border_all,
+              texto: 'General',
+            ),
+            _SingleCard(
+              color: Colors.pinkAccent,
+              icon: Icons.car_rental,
+              texto: 'Tramport',
+            ),
+          ],
+        ),
+        TableRow(
+          children: [
+            _SingleCard(
+              color: Colors.blue,
+              icon: Icons.border_all,
+              texto: 'General',
+            ),
+            _SingleCard(
+              color: Colors.pinkAccent,
+              icon: Icons.car_rental,
+              texto: 'Tramport',
+            ),
+          ],
+        ),
+        TableRow(
+          children: [
+            _SingleCard(
+              color: Colors.blue,
+              icon: Icons.border_all,
+              texto: 'General',
+            ),
+            _SingleCard(
+              color: Colors.pinkAccent,
+              icon: Icons.car_rental,
+              texto: 'Tramport',
+            ),
+          ],
+        ),
+        TableRow(
+          children: [
+            _SingleCard(
+              color: Colors.blue,
+              icon: Icons.border_all,
+              texto: 'General',
+            ),
+            _SingleCard(
+              color: Colors.pinkAccent,
+              icon: Icons.car_rental,
+              texto: 'Tramport',
+            ),
+          ],
+        ),
         TableRow(
           children: [
             _SingleCard(
@@ -42,30 +99,43 @@ class _SingleCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.all(15),
-      height: 180,
-      decoration: BoxDecoration(
-        color: const Color.fromRGBO(62, 67, 107, 0.7),
+      child: ClipRRect(
         borderRadius: BorderRadius.circular(20),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          CircleAvatar(
-            backgroundColor: color,
-            child: Icon(
-              icon,
-              size: 35,
+        child: BackdropFilter(
+          filter: ImageFilter.blur(
+            sigmaX: 5,
+            sigmaY: 5,
+          ),
+          child: Container(
+            // margin: const EdgeInsets.all(15),
+            height: 180,
+            decoration: BoxDecoration(
+              color: const Color.fromRGBO(62, 67, 107, 0.7),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  backgroundColor: color,
+                  child: Icon(
+                    icon,
+                    size: 35,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  texto,
+                  style: TextStyle(
+                    color: color,
+                    fontSize: 18,
+                  ),
+                ),
+              ],
             ),
           ),
-          const SizedBox(height: 10),
-          Text(
-            texto,
-            style: TextStyle(
-              color: color,
-              fontSize: 18,
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
